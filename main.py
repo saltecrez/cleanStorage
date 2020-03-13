6d4fc7ca21db593cfee5a0b3b677f01e048029a1
saltecrez <3lisa.londero@gmail.com>
2020-03-13 17:17:57 +0100

    main finalization

--- main.py
+++ main.py
@@ -5,16 +5,14 @@ __email__ = "elisa.londero@inaf.it"
 __date__ = "June 2018"
 
 import os
-#from glob import glob
-##from database import db_query
-#from utilities import md5Checksum
+from database import db_query
+from utilities import md5Checksum
 from datetime import datetime
 from datetime import timedelta
 from utilities import LoggingClass
 from read_json import ReadJson
-#from database import MySQLDatabase
-#from utilities import MissingConfParameter
-#from utilities import SendEmail
+from database import MySQLDatabase
+from utilities import SendEmail
 
 rj = ReadJson()
 log = LoggingClass('',True).get_logger()
@@ -24,7 +22,7 @@ class StorageFilesList(object):
         self.months = int(rj.get_months())
         self.strg_fldr = rj.get_storage_folder()
 
-    def create_list(self):
+    def create_storage_list(self):
         files_list = []
         for root,subdirs,files in os.walk(self.strg_fldr):
             for filename in files:
@@ -32,18 +30,14 @@ class StorageFilesList(object):
                 files_list.append(file_path) 
         return files_list
 
-    def select_dates(self):
-        date_in_past = [(datetime.now() + timedelta(-30*(self.months))).strftime('%Y/%m/%d')]
+    def create_selected_list(self):
+        limit_date = [(datetime.now() + timedelta(-30*(self.months))).strftime('%Y/%m/%d')]
         selected_list = []
-        for i in StorageFilesList().create_list():
+        for i in StorageFilesList().create_storage_list():
             date_from_path = os.path.relpath(i, self.strg_fldr)[:10]
-            if date_from_path < date_in_past[0]:
+            if date_from_path < limit_date[0]:
                 selected_list.append(i)
-        print(selected_list)
         return selected_list
-
-
-
 
 def main():
     dbuser = rj.get_db_user();  dbpwd = rj.get_db_pwd()   
@@ -56,30 +50,36 @@ def main():
 
     Session = db.create_session()
     
-    files_list = StorageFilesList().create_list()
+    files_list = StorageFilesList().create_selected_list()
 
     for file_path in files_list:
-        cks_newdata = md5Checksum(file_path).calculate_checksum()
-        fname = os.path.basename(os.path.splitext(os.path.normpath(file_path))[0])
-        fname_gz = fname + '.fits.gz' 
+        cksgz_stg = md5Checksum(file_path).calculate_checksum()
+        cks_stg = md5Checksum(file_path).get_checksum_gz()
+        head, fname_gz = os.path.split(file_path)
 
         for tbl in dbtables:
             db_element = db_query(tbl, Session, fname_gz) 
 
             if db_element is not None:
-                storage_path = db_element[0]
                 cks_db = db_element[1]
                 cksgz_db = db_element[2]
-                cksgz_stg = md5Checksum(storage_path).calculate_checksum() 
-                cks_stg = md5Checksum(storage_path).get_checksum_gz()
-                if cksgz_stg == cksgz_db and cks_stg == cks_db == cks_newdata:
-                    try:
-                        os.remove(file_path)
-                    except Exception as e:
-                        msg = "File removal exception --"
-                        log.error("{0}{1}".format(msg,e))  	
-                else: 
-                    message = 'Severe alert - newdata, storage and DB file checksums DO NOT MATCH'
+                if cks_db is not None and cksgz_db is not None:
+
+                    if cksgz_stg == cksgz_db and cks_stg == cks_db:
+
+                        try:
+                             print(file_path)
+                             #os.remove(file_path)
+                        except Exception as e:
+                            msg = "File removal exception --"
+                            log.error("{0}{1}".format(msg,e))  	
+   
+                    else:
+                        message = 'Severe alert - storage and DB file checksums DO NOT MATCH'
+                        SendEmail(message,recipient,sender,smtphost).send_email()
+    
+                else:
+                    message = 'Severe alert - checksum not calculated' 
                     SendEmail(message,recipient,sender,smtphost).send_email()
             else:
                 pass
@@ -87,6 +87,4 @@ def main():
     db.close_session()
 
 if __name__ == "__main__":
-    #main()
-    stor_path = '/home/archivio/tmp_storage/'
-    StorageFilesList().select_dates()
+    main()
